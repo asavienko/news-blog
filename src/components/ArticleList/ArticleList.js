81648b1c15e9c6495118991defb017f236a2a968
Anton <asavienko@gmail.com>
2020-10-25 20:32:40 +0200

    added infinity scroll

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,25 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Col, Row } from "antd";
+import InfiniteScroll from "react-infinite-scroller";
 import AppContext from "../../context/AppContext.js";
 import SmallArticleCard from "../SmallArticleCard";
 
 const ArticleList = () => {
   const { articleList } = useContext(AppContext);
+  const [slice, setSlice] = useState(20);
 
+  const loadFunc = () => {
+    setSlice(slice + 20);
+  };
   return (
-    <Row gutter={[0, 16]}>
-      {!!articleList.length &&
-        articleList.map(({ id, publicationDate, title, smallDescription }) => (
-          <Col xs={24} sm={12} md={12} lg={8} xl={6} key={id}>
-            <SmallArticleCard
-              id={id}
-              publicationDate={publicationDate}
-              title={title}
-              smallDescription={smallDescription}
-            />
-          </Col>
-        ))}
-    </Row>
+    <InfiniteScroll
+      styles={{ width: "100%" }}
+      loadMore={loadFunc}
+      hasMore={articleList.length > slice}
+    >
+      <Row gutter={[0, 16]}>
+        {!!articleList.length &&
+          articleList
+            .slice(0, slice)
+            .map(({ id, publicationDate, title, smallDescription }) => (
+              <Col xs={24} sm={12} md={12} lg={8} xl={6} key={id}>
+                <SmallArticleCard
+                  id={id}
+                  publicationDate={publicationDate}
+                  title={title}
+                  smallDescription={smallDescription}
+                />
+              </Col>
+            ))}
+      </Row>
+    </InfiniteScroll>
   );
 };
 
